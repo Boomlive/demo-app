1b288bbdc44e8fa4e0a8935ea6245b6ef7e17f30
Panuwith <boomlivecup@.com>
2023-12-12 14:26:58 +0700

    testjson

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello00")
+
+var data = {
+    id:1,
+    name:'Name1',
+    age:25
+}
+
+var jsonx = JSON.stringify(data);
+var datax = JSON.parse(jsonx);
+console.log(`json = ${jsonx}`);
+console.log(datax);
